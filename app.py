e4e1382a9db75fa015eb6326349cbf44afc26143
epixenpai <142733889+epixenpai@users.noreply.github.com>
2024-10-27 15:02:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from characterai import aiocai
 import asyncio
 from flask_cors import CORS
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -111,5 +111,6 @@ async def send_message():
 if __name__ == '__main__':
     # Initialize the CharacterAI client
     asyncio.run(init_client())
-    app.run(host='0.0.0.0',debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
